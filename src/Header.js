61ea391ddf1cc1f23326b3b052a86b775f671277
Sergey Skornyakov <s.o.skornyakov@gmail.com>
2017-05-10 15:36:23 +0300

    init commit

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Welcome to React</h2>
+    </div>
+  );
+};
