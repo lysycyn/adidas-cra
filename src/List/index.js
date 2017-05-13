b640785a0692050d80145da00b12228d964cd97b
Sergey Skornyakov <s.o.skornyakov@gmail.com>
2017-05-13 03:01:11 +0300

    fix cards

--- src/List/index.js
+++ src/List/index.js
@@ -1,65 +1,34 @@
-import React from "react"
-import { Grid, Row, Col } from "react-flexbox-grid"
-import Filter from "./Filter"
-import Card from "../components/Card"
-import { Wrapper, Hr } from "./styled"
+import React from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import Filter from './Filter';
+import Card from '../components/Card';
+import { Wrapper, Hr } from './styled';
 
-export default () => {
-  return (
-    <Wrapper>
-      <Filter />
-      <Hr />
-      <Grid fluid>
-        <Row>
-          <Col xs={12} sm={6} md={4} lg={3}>
-            <Card
-              isSale
-              src={require("../assets/img/boot1.jpg")}
-              price="170$"
-              to="/item"
-            />
-          </Col>
-          <Col xs={12} sm={6} md={4} lg={3}>
-            <Card
-              src={require("../assets/img/boot2.jpg")}
-              price="170$"
-              to="/item"
-            />
-          </Col>
-          <Col xs={12} sm={6} md={4} lg={3}>
-            <Card
-              isSale
-              src={require("../assets/img/boot1.jpg")}
-              price="170$"
-              to="/item"
-            />
-          </Col>
-          <Col xs={12} sm={6} md={4} lg={3}>
-            <Card
-              isSale
-              src={require("../assets/img/boot2.jpg")}
-              price="170$"
-              to="/item"
-            />
-          </Col>
-          <Col xs={12} sm={6} md={4} lg={3}>
-            <Card
-              isSale
-              src={require("../assets/img/boot2.jpg")}
-              price="170$"
-              to="/item"
-            />
-          </Col>
-          <Col xs={12} sm={6} md={4} lg={3}>
-            <Card
-              isSale
-              src={require("../assets/img/boot1.jpg")}
-              price="170$"
-              to="/item"
-            />
-          </Col>
-        </Row>
-      </Grid>
-    </Wrapper>
-  )
-}
+export default () => (
+  <Wrapper>
+    <Filter />
+    <Hr />
+    <Grid fluid>
+      <Row>
+        <Col xs={12} sm={6} md={4} lg={3}>
+          <Card isSale src={require('../assets/img/boot1.jpg')} price="170$" to="/item" />
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3}>
+          <Card src={require('../assets/img/boot2.jpg')} price="170$" to="/item" />
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3}>
+          <Card isSale src={require('../assets/img/boot1.jpg')} price="170$" to="/item" />
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3}>
+          <Card isSale src={require('../assets/img/boot2.jpg')} price="170$" to="/item" />
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3}>
+          <Card isSale src={require('../assets/img/boot2.jpg')} price="170$" to="/item" />
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3}>
+          <Card isSale src={require('../assets/img/boot1.jpg')} price="170$" to="/item" />
+        </Col>
+      </Row>
+    </Grid>
+  </Wrapper>
+  );
