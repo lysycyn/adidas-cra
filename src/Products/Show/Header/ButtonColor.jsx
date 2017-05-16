95e58c3a67731d79c8012a3e26a9c984e608ad51
Sergey Lysycyn <lysycyn@gmail.com>
2017-05-16 12:48:34 +0300

    fix: merge branch

---
+++ src/Products/Show/Header/ButtonColor.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import media from '../../../styled-components/media';
+
+export default styled.button`
+  display: inline-block;
+  margin-right: 1.2rem;
+  font-size: 0;
+  width: 2.25rem;
+  height: 2.25rem;
+  border-radius: 1.125rem;
+  ${media.lg('min')`
+    width: 1.8rem;
+    height: 1.8rem;
+    border-radius: .9rem;
+  `}
+  background: ${props => props.color};
+`;
---
+++ src/Products/Show/Header/Price.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export default styled.h2`
+  margin: 0;
+  font-family: "avenir";
+  font-weight: 700;
+  font-size: 10rem;
+  color: #e2e2e2;
+  &:after{
+    content: "$";
+  }
+`;
